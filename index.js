930f8a5715c3d06752b914fd6cd3d83b0b9ee1c6
Nicholas Barosso <nbarosso@gmail.com>
2023-07-27 18:27:35 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-const input = document.getElementById('button');
-input.addEventListener('click', function() {
-  alert('I was clicked!');
-});
-
 function addingEventListener() {
-    const input = document.getElementById('button');
+const input = document.getElementById('button');
 
 function clickAlert() {
   alert('I was clicked!');
@@ -12,3 +7,8 @@ function clickAlert() {
 
 input.addEventListener('click', clickAlert);
 }
+
+const input = document.getElementById('button');
+input.addEventListener('click', function() {
+  alert('I was clicked!');
+});
